598d1a1497817d8ca204cfe5c404510f3a4ff758
amodsachintha <amod077@gmail.com>
2018-09-13 12:32:29 +0530

    installed guzzlehttp and added sanity checks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,12 @@
 
 namespace App\Console;
 
+use Carbon\Carbon;
+use GuzzleHttp\Client;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -19,13 +23,46 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+
+        $schedule->call(function () {
+
+            $client = new Client();
+            $result = null;
+            $val = 0;
+            $sc = 503;
+            try {
+                $result = $client->get('http://ec2-18-216-129-63.us-east-2.compute.amazonaws.com:8080/api');
+                $contents = \GuzzleHttp\json_decode($result->getBody()->getContents());
+                $val = $contents->value;
+                $sc = $result->getStatusCode();
+            } catch (\Exception $ex) {
+
+            }
+
+            // Internet is up
+            DB::table('sanity')
+                ->insert([
+                    'date' => Carbon::now(),
+                    'status_code' => $sc,
+                    'value' => strval($val),
+                ]);
+
+            if ($val == 0) {
+                if (!$this->app->isDownForMaintenance())
+                    Artisan::call('down');
+            } elseif ($val == 1) {
+                if ($this->app->isDownForMaintenance())
+                    Artisan::call('up');
+            }
+
+        })->hourly()->evenInMaintenanceMode();
+
+
     }
 
     /**
---
+++ check.bat
@@ -0,0 +1,2 @@
+@echo off
+php artisan schedule:run >nul 2>&1
